9cba70ab24f5a53699f8c3571bdd1dfbacd1107f
Borked Engineer 🤓 <borkedengineer@gmail.com>
2024-10-11 23:26:31 -0700

    Remove comments from script lol

--- scripts/phat.py
+++ scripts/phat.py
@@ -1,10 +1,8 @@
 import webbrowser
 
-# URL of the website you want to open
 url = "https://www.example.com"
 
 try:
-    # Attempt to open the URL in the default web browser
     webbrowser.open(url)
     print(f"Opening {url} in your default web browser.")
 except webbrowser.Error as e:
