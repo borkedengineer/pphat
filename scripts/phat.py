dd459a574a49ffdffa1c10a82d4a2e638b79932d
Anusan Siva 🤓 <borkedengineer@gmail.com>
2025-03-09 23:43:07 -0700

    Remove unused python script

--- scripts/phat.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-
-url = "https://www.faa.gov"
-
-try:
-    webbrowser.open(url)
-    print(f"Opening {url} in your default web browser.")
-except webbrowser.Error as e:
-    print(f"An error occurred: {e}")
